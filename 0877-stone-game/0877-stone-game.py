72b634a3c742638be9edfe2c7a2526eb96f1abfe
anushka-y2 <anuanu1884@gmail.com>
2026-08-06 14:38:39 +0530

    Time: 173 ms (12.15%), Space: 17.4 MB (10.5%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        n = len(piles)
+        dp = [[0] * n for _ in range(n)]
+        
+        # base case: single pile
+        for i in range(n):
+            dp[i][i] = piles[i]
+        
+        # fill by increasing interval length
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1])
+        
+        return dp[0][n-1] > 0
